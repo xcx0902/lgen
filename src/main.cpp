5943f667ff753da324d060eb2f29be2a8e1dfd70
xcx0902 <xcx0902@126.com>
2023-01-28 20:37:02 +0800

    🌍upd: 更新 exe 标题版本号

--- src/main.cpp
+++ src/main.cpp
@@ -96,7 +96,7 @@ inline void selectMap() {
 
 int main() {
     ShowWindow(hwnd, SW_MAXIMIZE);
-    system("title Local Generals v1.1.1");
+    system("title Local Generals v1.1.2");
     initattr();
     hideCursor();
     printf("Welcome to Local Generals!\n");
