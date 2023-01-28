bc50006e9d0d5aaf15ca326b8d1a829651a61293
xcx0902 <xcx0902@126.com>
2023-01-28 15:28:10 +0800

    🌈style: 将 Tab 修改为 4 个空格

--- src/defs.h
+++ src/defs.h
@@ -16,31 +16,31 @@
 std::mt19937 mtrand(std::chrono::system_clock::now().time_since_epoch().count());
 
 inline int random(int k) {
-	return mtrand() % k;
+    return mtrand() % k;
 }
 
 inline int random(int l, int r) {
-	return mtrand() % (r - l + 1) + l;
+    return mtrand() % (r - l + 1) + l;
 }
 
 // Console functions
 
 inline void getxy(int& x, int& y) {
-	CONSOLE_SCREEN_BUFFER_INFO bInfo;
-	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &bInfo);
-	y = bInfo.dwCursorPosition.X;
-	x = bInfo.dwCursorPosition.Y;
+    CONSOLE_SCREEN_BUFFER_INFO bInfo;
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &bInfo);
+    y = bInfo.dwCursorPosition.X;
+    x = bInfo.dwCursorPosition.Y;
 }
 
 inline void clearall() {
-	HANDLE hdout = GetStdHandle(STD_OUTPUT_HANDLE);
-	CONSOLE_SCREEN_BUFFER_INFO csbi;
-	GetConsoleScreenBufferInfo(hdout, &csbi);
-	DWORD size = csbi.dwSize.X * csbi.dwSize.Y, num = 0;
-	COORD pos = {0, 0};
-	FillConsoleOutputCharacter(hdout, ' ', size, pos, &num);
-	FillConsoleOutputAttribute(hdout, csbi.wAttributes, size, pos, &num);
-	SetConsoleCursorPosition(hdout, pos);
+    HANDLE hdout = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    GetConsoleScreenBufferInfo(hdout, &csbi);
+    DWORD size = csbi.dwSize.X * csbi.dwSize.Y, num = 0;
+    COORD pos = {0, 0};
+    FillConsoleOutputCharacter(hdout, ' ', size, pos, &num);
+    FillConsoleOutputAttribute(hdout, csbi.wAttributes, size, pos, &num);
+    SetConsoleCursorPosition(hdout, pos);
 }
 inline void clearline() { fputs("\033[K", stdout); }
 inline void gotoxy(int x, int y) { printf("\033[%d;%dH", x, y); }
@@ -59,16 +59,16 @@ inline void setbcolor(int R, int G, int B) { printf("\033[48;2;%d;%d;%dm", R, G,
 inline void underline() { fputs("\033[4m", stdout); }
 
 inline int initattr() {
-	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-	if(hOut == INVALID_HANDLE_VALUE)
-		return GetLastError();
-	DWORD dwMode = 0;
-	if(!GetConsoleMode(hOut, &dwMode))
-		return GetLastError();
-	dwMode |= 0x0004;
-	if(!SetConsoleMode(hOut, dwMode))
-		return GetLastError();
-	return 0;
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    if(hOut == INVALID_HANDLE_VALUE)
+        return GetLastError();
+    DWORD dwMode = 0;
+    if(!GetConsoleMode(hOut, &dwMode))
+        return GetLastError();
+    dwMode |= 0x0004;
+    if(!SetConsoleMode(hOut, dwMode))
+        return GetLastError();
+    return 0;
 }
 
 inline void resetattr() { fputs("\033[0m\033[38;2;255;255;255m", stdout); }
@@ -81,7 +81,7 @@ struct block {
     ll army;
 };
 
-int R, C, delay, cheatCode, players;
+int R, C, delay, cheatCode, players, messages;
 const int MAX_SIZE = 500;
 block map[MAX_SIZE + 5][MAX_SIZE + 5];
 
@@ -91,33 +91,38 @@ struct defTeam {
 };
 
 defTeam team[64] = {
-	{"White", 0xffffff},
-	{"Red", 0xff0000},
-	{"Green", 0x00ff00},
-	{"Blue", 0x0000ff},
-	{"Yellow", 0xffff00},
-	{"Cyan", 0x00ffff},
-	{"Rose", 0xff00ff},
-	{"Orange", 0xff8000},
-	{"Lime", 0x80ff00},
-	{"Brown", 0x804000},
-	{"Grey", 0x808080},
-	{"Teal", 0x008080},
-	{"Purple", 0xc000c0},
-	{"Silver", 0xc0c0c0},
-	{"Maroon", 0xc00000},
-	{"Emerald", 0x00ce80},
-	{"Olive", 0x808000},
+    {"White", 0xffffff},
+    {"Red", 0xff0000},
+    {"Green", 0x00ff00},
+    {"Blue", 0x0000ff},
+    {"Yellow", 0xffff00},
+    {"Cyan", 0x00ffff},
+    {"Rose", 0xff00ff},
+    {"Orange", 0xff8000},
+    {"Lime", 0x80ff00},
+    {"Brown", 0x804000},
+    {"Grey", 0x808080},
+    {"Teal", 0x008080},
+    {"Purple", 0xc000c0},
+    {"Silver", 0xc0c0c0},
+    {"Maroon", 0xc00000},
+    {"Emerald", 0x00ce80},
+    {"Olive", 0x808000},
 };
 
 void printTeam(int id) {
-	setfcolor(team[id].color);
-	printf("%s", team[id].name.c_str());
-	resetattr();
+    setfcolor(team[id].color);
+    printf("%s", team[id].name.c_str());
+    resetattr();
 }
 
 struct defPlayer {
     int x, y;
+};
+
+struct defMessage {
+    int turn, id;
+    std::string msg;
 };
 
 const char suf[20] = {0, 'H', 'K', 'W', 'L', 'M', 'Q', 'I', 'G', 'B', 'N', 'T'};
