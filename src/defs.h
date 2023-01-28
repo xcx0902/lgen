c2cb65e9c0dc989f8462c7f7f803476bb3086f6e
xcx0902 <xcx0902@126.com>
2023-01-28 13:16:45 +0800

    🌍upd: 新增 delays (倍速)

--- src/defs.h
+++ src/defs.h
@@ -114,5 +114,6 @@ struct defPlayer {
 };
 
 const char suf[20] = {0, 'H', 'K', 'W', 'L', 'M', 'Q', 'I', 'G', 'B', 'N', 'T'};
+const char* delays[10] = {0, "1x", "2x", "4x", "8x", "16x", "32x", "FAST"};
 
 #endif // __LGEN_DEFS_H
