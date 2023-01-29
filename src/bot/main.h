23521a1c4daa4b5cd3b13db3ef23a24af40ba044
xcx0902 <xcx0902@126.com>
2023-01-29 20:42:01 +0800

    🌍upd(bot): 添加新 bot (LG-dev/LG-new/Bot/ktqBot.hpp) 并替换 mainBot

--- src/bot/main.h
+++ src/bot/main.h
@@ -6,30 +6,32 @@
 
 namespace mainBot {
     int mainBot(int id, defPlayer pos) {
-        if (map[pos.x][pos.y].belong != id || map[pos.x][pos.y].army == 0)
-            return 0;
+        static int swampDir[20] = {3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3};
+        if (map[pos.x][pos.y].belong != id || map[pos.x][pos.y].army == 0) return 0;
         struct node {
-            int type, team, dir;
-            ll army;
-        } p[5];
+            int to, belong, type;
+            ll army, del;
+            bool operator < (node b) {
+                return army < b.army || (army == b.army && del < b.del);
+            }
+        };
+        node p[10];
         int cnt = 0;
         for (int i = 1; i <= 4; i++) {
-            if (pos.x + dx[i] < 1 || pos.x + dx[i] > R || pos.y + dy[i] < 1 || pos.y + dy[i] > C || map[pos.x + dx[i]][pos.y + dy[i]].type == 2) continue;
-            p[++cnt] = {map[pos.x + dx[i]][pos.y + dy[i]].type, map[pos.x + dx[i]][pos.y + dy[i]].belong, i, map[pos.x + dx[i]][pos.y + dy[i]].army};
+            int tx = pos.x + dx[i], ty = pos.y + dy[i];
+            if (map[tx][ty].type == 2 || tx < 1 || tx > R || ty < 1 || ty > C) continue;
+            p[++cnt] = {i, map[tx][ty].belong, map[tx][ty].type, map[tx][ty].army, map[tx][ty].army};
+            if (p[cnt].type != 1 && p[cnt].belong == id) p[cnt].army = -p[cnt].army, p[cnt].del = -p[cnt].del;
+            if (p[cnt].type == 4 && p[cnt].belong != id) p[cnt].army = 2 * p[cnt].army - (ll)(1e15);
+            else if (p[cnt].type == 0 && p[cnt].belong != id) p[cnt].army = p[cnt].army - (ll)(1e15);
+            else if (p[cnt].type == 1) p[cnt].del = 200, p[cnt].army = -1e16;
+            else if (p[cnt].type == 3 && p[cnt].belong != id) p[cnt].army = -(ll)(1e18);
         }
-        bool rd = random(2);
-        auto cmp = [&](node a, node b) -> bool {
-            if (a.type == 3 && a.team != id) return true;
-            if (b.type == 3 && b.team != id) return false;
-            if (a.team == 0) return rd;
-            if (b.team == 0) return !rd;
-            if (a.team == id && b.team != id) return false;
-            if (a.team != id && b.team == id) return true;
-            if (a.team == id && b.team == id) return a.army > b.army;
-            return a.army < b.army;
-        };
-        std::sort(p + 1, p + cnt + 1, cmp);
-        return p[1].dir;
+        std::sort(p + 1, p + cnt + 1);
+        for (int i = 1; i <= cnt; i++)
+            if (p[i].del < map[pos.x][pos.y].army)
+                return p[i].to;
+        return -1;
     }
 }
 
